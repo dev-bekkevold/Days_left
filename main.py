6b7c14c301cc034801f222d51740fe227b30d74d
Dev Bekkevold <dev.bekkevold@gmail.com>
2022-01-25 21:13:15 +0100

    Make variables and functions

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(f"20 days are {20 * 24 * 60} minutes")
+calculation_to_hours = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days): 
+    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+
+
+user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
+
