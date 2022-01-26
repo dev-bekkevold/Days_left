2dcf263c47752ef1091a126ee3c48b94584f97ea
Dev Bekkevold <dev.bekkevold@gmail.com>
2022-01-26 20:26:39 +0100

    Add if/else statements

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days): 
-    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You entered a 0, please enter a valid positive number."
+    else:
+        return "You entered a negative value, so no conversion for you!"
 
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
