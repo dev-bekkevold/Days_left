149fd265f8ffe843851153ed1704aa9f067f9faf
Dev Bekkevold <dev.bekkevold@gmail.com>
2022-01-27 20:56:43 +0100

    Validate the input, clean up my code

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days): 
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
-    elif num_of_days == 0:
-        return "You entered a 0, please enter a valid positive number."
-    else:
-        return "You entered a negative value, so no conversion for you!"
+    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    
 
+def validate_and_execute():
+    try:
+
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered a 0, please enter a valid positive number.")       
+        else:
+            print("You entered a negative number, no conversion for you!")
+            
+    except ValueError:
+            print("Your input is not a number, don't ruin my program!")
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-user_input_number = int(user_input)
-
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
+validate_and_execute()
 
